ddc82179f645739103b72622666c1d43f031c298
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2024-08-28 19:57:58 +0200

    Revert "update: Add cocoapods"
    This reverts commit 8bd784a2966a11f6820b2eb94193d24be4fd8a26.

--- composeApp/src/iosMain/kotlin/com/andruszkiewiczarturmobileeng/planyourprotocol/data/AppDatabase.kt
+++ composeApp/src/iosMain/kotlin/com/andruszkiewiczarturmobileeng/planyourprotocol/data/AppDatabase.kt
@@ -2,33 +2,16 @@ package com.andruszkiewiczarturmobileeng.planyourprotocol.data
 
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import platform.Foundation.NSDocumentDirectory
-import platform.Foundation.NSFileManager
 import platform.Foundation.NSHomeDirectory
-import platform.Foundation.NSUserDomainMask
 
 fun getDatabaseBuilder(): ProtocolDatabase {
-    val dbFile = "${documentDirectory()}/protocolDB.db"
+    val dbFile = "${NSHomeDirectory()}/protocolDB.db"
     return Room.databaseBuilder<ProtocolDatabase>(
         name = dbFile,
         factory = { ProtocolDatabase::class.instantiateImpl() }
     ).setDriver(BundledSQLiteDriver())
         .setQueryCoroutineContext(Dispatchers.IO)
         .build()
-}
-
-@OptIn(ExperimentalForeignApi::class)
-private fun documentDirectory(): String {
-    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
-        directory = NSDocumentDirectory,
-        inDomain = NSUserDomainMask,
-        appropriateForURL = null,
-        create = false,
-        error = null
-    )
-
-    return requireNotNull(documentDirectory?.path)
 }
