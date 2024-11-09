8abfcb3b8aab1c09877e99099891aefcb8b6f8e5
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2024-11-09 02:01:44 +0100

    fix: Bug with preferences

--- composeApp/src/iosMain/kotlin/com/andruszkiewiczarturmobileeng/planyourprotocol/CreateDataStore.kt
+++ composeApp/src/iosMain/kotlin/com/andruszkiewiczarturmobileeng/planyourprotocol/CreateDataStore.kt
@@ -1,9 +1,6 @@
 package com.andruszkiewiczarturmobileeng.planyourprotocol
 
 import com.andruszkiewiczarturmobileeng.planyourprotocol.core.DATA_STORE_FILE_NAME
-
-@file:OptIn(ExperimentalForeignApi::class)
-
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -11,6 +8,7 @@ import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> {
     return com.andruszkiewiczarturmobileeng.planyourprotocol.core.createDataStore {
         val directory = NSFileManager.defaultManager.URLForDirectory(
